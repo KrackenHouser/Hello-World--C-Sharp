2e7b15bf44909c8aa91ba2f88d75286ced131905
KrackenHouser <dawnsss23@proton.me>
2024-06-05 02:34:53 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Hello Wordl!\n");
+        }
+        finally
+        {
+            ExitWithMessage();
+        }
+    }
+    static void ExitWithMessage()
+    {
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(); // Espera a que el usuario presione una tecla
+        Environment.Exit(0); // Salir del programa con código de salida 0 (éxito)
+    }
+}
